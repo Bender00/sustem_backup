057dcb17fdbd4f998885dca917df30a57b214daa
Bender Rodriguez <andriitukhonov@gmail.com>
2021-04-19 19:23:45 +0300

    Збір інформації із машини. Створення та занесення цих данних у файл info_machines.txt

---
+++ os_sys.py
@@ -0,0 +1,21 @@
+import os, sys
+
+# os.name - виводить назву операційної системи. nt - windows, posix - linux, java - java
+# sys.platform - шдентифікатор платформи.
+# os.getcwd - вивотить шлях до данного файлу
+
+
+
+
+#print(os.environ['USERNAME'])
+#print(os.environ['LOGNAME'])
+#print(os.environ['HOME'])
+#print(os.environ['WINDOWPATH'])
+#print(os.stat(path))
+#print(os.uname())
+#print(os.name)
+#print(os.getcwd())
+#for environ in os.environ:
+#    print(environ)
+
+
