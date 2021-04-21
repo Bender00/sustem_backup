089ac225d7eb825e7e52fc6c021551435d30fb2d
Bender Rodriguez <andriitukhonov@gmail.com>
2021-04-21 20:35:42 +0300

    Виправлені неточності. Винесені функції у файл function.py

--- backup.py
+++ backup.py
@@ -1,37 +1,19 @@
-from datetime import datetime
-import os
-import sys
-import uuid # Для виводу MAC - адресу
+from function import *
 
-#вивод актуальної дати
-def now_date():
-    now = datetime.now()
-    date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
-    return now
+# вивод актуальної дати
+# now_date()
 
-
-#Виводить MAC-адрес
-def mac_address():
-    print ("The MAC address in formatted way is : ", end="mac")
-    mac = (':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff)
-    for ele in range(0,8*6,8)][::-1]))
-    return mac
-
-mac = mac_address()
+# Виводить MAC-адрес
+# mac_address()
 
 # інфа про систему
-OS = os.uname()
-info_machines = {
-    'sysname' : OS.sysname,
-    'nodename' : OS.nodename,
-    'releace' : OS.release,
-    'version' : OS.version
-}
+# info_machines()
 
-
+# Відкриття(створення) файла
 file_info = open("info_machines.txt", mode='a', encoding='utf-8')
 
-file_info.write('\n' + str(mac) + '\t' + str(now_date()) + '\n' + '\t' + str(info_machines))
+# Записує у файл дані із змінної OS та з функції mac_address() + now_date()
+file_info.write('\n' + str(mac_address()) + '\t' + str(now_date()) + '\n' + '\t' + str(info_machines()))
 
 #with file_info as file:
 #    for item in info_machines:
@@ -43,9 +25,3 @@ file_info.write('\n' + str(mac) + '\t' + str(now_date()) + '\n' + '\t' + str(inf
 #    file_info.write("%s\n" % item)
 
 file_info.close()
-
-if OS.sysname == 'Linux' :
-    print('linux')
-    print(os.uname())
-else:
-    print('Ти рак на вінді')
